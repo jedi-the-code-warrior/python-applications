75ac0a55a581bc3118324638554d2ab4452e286c
A K Shiwakoti <40875839+jedi-thecodewarrior@users.noreply.github.com>
2019-03-01 01:32:03 -0800

    Update ps4a.py

--- ps4a.py
+++ ps4a.py
@@ -15,7 +15,7 @@ SCRABBLE_LETTER_VALUES = {
 # Helper code
 # (you don't need to understand this helper code)
 
-WORDLIST_FILENAME = "words.txt"
+WORDLIST_FILENAME = "datasets/words_scrabble.txt"
 
 def loadWords():
     """
