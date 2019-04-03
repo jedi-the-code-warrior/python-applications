9ac2b2c847874b8976993b36396fd9856541195f
A K Shiwakoti <40875839+jedi-the-code-warrior@users.noreply.github.com>
2019-04-03 05:01:45 -0700

    Update polynomial_sum.py

--- polynomial_sum.py
+++ polynomial_sum.py
@@ -5,12 +5,16 @@ Created on Tue Feb 26 04:38:41 2019
 @author: Anjani K Shiwakoti
 """
 
-def general_poly (L):
+def general_poly (L,x=2):
     
     """ 
+    x = 2, variable is initialized here with base 2 but can be any integer
     L, a list of numbers or coefficients (n0, n1, n2, ... nk)
-    Returns a function, which when applied to a value x, returns the value 
-    n0 * x^k + n1 * x^(k-1) + ... nk * x^0 
+    Returns a function, which when applied to a value x, returns the
+    expression: n0 * x^k + n1 * x^(k-1) + ... nk * x^0 
+    values: val(k)+ val(k-1) + ... +val(0)
+    sum: sum of all values
+    
     """
     def translate_string(any_string, super_or_sub):
         if super_or_sub == 'SUB':
@@ -20,16 +24,16 @@ def general_poly (L):
     
         return any_string.translate(to_string)
     
-    def func_in_poly(x):
+    def func_in_poly(num_count):
     
-        num_count = len(L)
+        k = num_count
         
         poly_list = []
         poly_expr = []
         
-        for i in range(num_count):
+        for i in range(k):
             coefficient = L[i]
-            exponent = (num_count-1)-i
+            exponent = (k-1)-i
             poly_list.append( coefficient * (x**exponent))
             
             poly_expr.append(str(coefficient) + "x" + translate_string(str(exponent),'SUP'))
@@ -38,8 +42,8 @@ def general_poly (L):
     
     return func_in_poly(len(L))
 
-# get all values from above functions
-polynomial_expression, polynomials, polynomial_sum = (general_poly ([1,5,9,2,6,7,3,8,4]))
+# grab all values from above functions and display
+polynomial_expression, polynomials, polynomial_sum = general_poly ([1,5,9,2,6,7,3,8,4])
 print()
 print ("Polynomial Sum = ", end ='') 
 for element in polynomial_expression:
@@ -53,11 +57,12 @@ for element in polynomials:
     if element != polynomials[-1]:
         print (' + ', end='')
 print()
-print ("Polynomial Sum = ", polynomial_sum)
- 
+print ("Polynomial Sum =",polynomial_sum)
+
+#--------------------------------------------------------------------------------------------------------------------------------------
 """
 OUTPUT: 
 Polynomial Sum = 1x⁸ + 5x⁷ + 9x⁶ + 2x⁵ + 6x⁴ + 7x³ + 3x² + 8x¹ + 4x⁰
-Polynomial Sum = 43046721 + 23914845 + 4782969 + 118098 + 39366 + 5103 + 243 + 72 + 4
-Polynomial Sum =  71907421
+Polynomial Sum = 256 + 640 + 576 + 64 + 96 + 56 + 12 + 16 + 4
+Polynomial Sum = 1720
 """
