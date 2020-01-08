a66000cc11c42980f36e88c15303268cbe033b62
A K Shiwakoti <40875839+jedi-the-code-warrior@users.noreply.github.com>
2020-01-08 11:15:52 -0800

    Update polynomial_sum.py

--- polynomial_sum.py
+++ polynomial_sum.py
@@ -3,12 +3,15 @@
 Created on Tue Feb 26 04:38:41 2019
 
 @author: Anjani K Shiwakoti
+
+This is my own method for generating polynomial expressions and their sums.
+
 """
 
 def general_poly (L,x=2):
     
     """ 
-    x = 2, variable is initialized here with base 2 but can be any integer
+    x = 2, variable is initialized with base 2 but can be overridden by any integer
     L, a list of numbers or coefficients (n0, n1, n2, ... nk)
     Returns a function, which when applied to a value x, returns the
     expression: n0 * x^k + n1 * x^(k-1) + ... nk * x^0 
@@ -45,13 +48,13 @@ def general_poly (L,x=2):
 # grab all values from above functions and display
 polynomial_expression, polynomials, polynomial_sum = general_poly ([1,5,9,2,6,7,3,8,4])
 print()
-print ("Polynomial Sum = ", end ='') 
+print ("Polynomial Expression = ", end ='') 
 for element in polynomial_expression:
     print (element, end='')
     if element != polynomial_expression[-1]:
         print (' + ', end='')
 print()
-print ("Polynomial Sum = ", end ='')   
+print ("Polynomial Interim = ", end ='')   
 for element in polynomials:
     print (element, end='')
     if element != polynomials[-1]:
@@ -62,7 +65,7 @@ print ("Polynomial Sum =",polynomial_sum)
 #--------------------------------------------------------------------------------------------------------------------------------------
 """
 OUTPUT: 
-Polynomial Sum = 1x⁸ + 5x⁷ + 9x⁶ + 2x⁵ + 6x⁴ + 7x³ + 3x² + 8x¹ + 4x⁰
-Polynomial Sum = 256 + 640 + 576 + 64 + 96 + 56 + 12 + 16 + 4
+Polynomial Expression = 1x⁸ + 5x⁷ + 9x⁶ + 2x⁵ + 6x⁴ + 7x³ + 3x² + 8x¹ + 4x⁰
+Polynomial Interim = 256 + 640 + 576 + 64 + 96 + 56 + 12 + 16 + 4
 Polynomial Sum = 1720
 """
