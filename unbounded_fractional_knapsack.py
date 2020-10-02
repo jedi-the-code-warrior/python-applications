422d44dfafe5ff8f2c85b6819bc31b27a949e972
A K Shiwakoti <40875839+jedi-the-code-warrior@users.noreply.github.com>
2020-10-02 12:41:24 -0700

    Add files via upload
    Unbounded fractional knapsack problem solution

---
+++ unbounded_fractional_knapsack.py
@@ -0,0 +1,46 @@
+### Unbounded fractional knapsack
+
+from collections import OrderedDict
+
+def fractional_knapsack(values, weights, capacity):
+	""" 
+	computes fractional knapsack, unbounded (can repeat weights) 
+	"""
+	keys = range(len(values))
+	ratios = [v/w for v,w in zip(values,weights)]
+	print(ratios)
+	bag = dict(zip(keys,ratios))
+	print(bag)
+	sorted_bag = dict(sorted(bag.items(), key=lambda x: x[1], reverse=True))
+
+	print(sorted_bag)
+	max_value = 0.0
+	fractions = [0.0] * len(values)
+
+	for key,ratio in sorted_bag.items():
+		#print(weights[key])
+
+		while weights[key] <= capacity:
+			max_value += values[key]
+			capacity -= weights[key]
+			fractions[key] += 1
+		else:
+			fractions[key] += (capacity/weights[key])
+			max_value += (values[key] * capacity/weights[key])
+			break
+			
+
+	return max_value, fractions 
+
+
+
+
+### driver code: 
+
+weights = [5, 1, 2, 4, 3, 10]
+values = [10, 100, 201, 401, 400, 1]
+capacity = 50
+
+max_value, fractions = fractional_knapsack(values, weights, capacity)
+print('The maximum value of items that can be carried:', max_value)
+print('The fractions in which the items should be taken:', fractions)
