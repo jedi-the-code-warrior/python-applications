ab8067ba95df00a0f736094bccef880e448fd625
A K Shiwakoti <40875839+jedi-the-code-warrior@users.noreply.github.com>
2020-10-10 08:31:43 -0700

    Update validate_email_and_phone_using_regex.py

--- validate_email_and_phone_using_regex.py
+++ validate_email_and_phone_using_regex.py
@@ -2,16 +2,7 @@
 import re
 import string
 
-# reviews = ["I don't know about you but I like Apple better than Google. Apple has better employee satisfaction than Google.",
-#            "Microsoft is also a great place to work but I sometimes don't like their policies. I'd prefer NetFlix if I could choose between FAANGs.",
-#            "Facebook is also one of my dream places to work. I admire Facebook's employee compensation plans but lately I am hesitant to apply at Facebook because of thier political involvement regarding consumer privacy.",
-#            "Microsoft and Amazon are among my top 5 places to work, but if I were given a choice, I would choose one from Google, Apple, LinkedIn, and NetFlix.",
-#            "Apple is definitely the best place to work for me because I personally like and use their products and Apple has always stood out as a cool company."]
-
-# for quote in reviews:
-#     word_list = re.sub(r'[^\w=]', ' ',quote)
-
-#     print(word_list)
+### some useful string manipulation functions
 
 alpha = string.ascii_letters
 print(alpha)
@@ -21,6 +12,7 @@ digits = string.digits
 print(digits)
 punc = string.punctuation
 print(punc)
+
 phone_cleaned=''
 phone_cleaned_list=[]
 
